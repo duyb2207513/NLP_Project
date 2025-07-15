91737959f5fbeea77ccec2c01084bc52bda29954
Than Quoc Tuan <tuanB2207574@student.ctu.edu.vn>
2025-07-15 16:45:12 +0700

    Them roberta

--- backend/routes/roberta_prediction.py
+++ backend/routes/roberta_prediction.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, jsonify
-from models import roberta_model,roberta_tokenizer
+from models import roberta_model,roberta_tokenizer, model_sumary, tokenize_sumary
 from utils.classificate import classificate
-
+from utils.sumarize import sumary_text
+from utils.find_related import find_related_links
 
 roberta_route = Blueprint('roberta_route', __name__)
 
@@ -10,13 +11,19 @@ def predict_roberta():
     try:
         data = request.json
         result = classificate(roberta_tokenizer,roberta_model,data['text'])
-       
+        sumaried_text = sumary_text(tokenize_sumary,model_sumary,data['text'])
+        find_related_link= find_related_links(data['text'])
+        
         return jsonify({
             "success": True,
-            "data": result,
+            "data": {
+                "label": result,
+                "summary": sumaried_text,
+                "links": find_related_link
+            },
             "message": "Dự đoán thành công"
         }), 200
-       
+        
     except Exception as e:
             return jsonify({
                 "success": False,
--- backend/utils/find_related.py
+++ backend/utils/find_related.py
@@ -7,7 +7,7 @@ def find_related_links(query, num_results=3):
         print(" Đang tìm kiếm với từ khoá:", query)
         for url in search(query, num_results=num_results, lang="vi"):
             print(" Tìm thấy URL:", url)  # dòng này giúp debug
-            if any(source in url for source in ['vnexpress.net', 'tuoitre.vn', 'vtv.vn', 'moit.gov.vn']):
+            if any(source in url for source in ['vnexpress.net', 'tuoitre.vn', 'vtv.vn', 'moit.gov.vn','thanhnien.vn']):
                 related_links.append(url)
     except Exception as e:
         print(" Lỗi:", e)
